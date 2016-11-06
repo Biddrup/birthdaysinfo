bbe07a7229a061d9cb5dfee0b63ead1319452232
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-07 01:25:42 +0600

    Update birthdaysinfo.py

--- birthdaysinfo.py
+++ birthdaysinfo.py
@@ -1,4 +1,4 @@
-#This wii keep the database
+#This will keep the database
 birthdays = {}
 
 #This will help you to find out and
@@ -6,21 +6,34 @@ birthdays = {}
 while True:
     #This will take input from user
     print('Enter a name: (blank to quit)')
+
     #keep in mind input is casesensetive
     #so, 'Biddrup' and 'biddrup' is different from each other
     name = input()
+
     #If input '', then loop will be terminated
     if name == '':
         break
+    #Will make each input as uppercase
+    newname = name.upper()
+
     #This will check the name in databse or not
-    if name in birthdays:
+    if newname in birthdays:
+        
         #If the input in databse, this will print
-        print (birthdays[name] + ' is the birthday of ' + name)
+        print (birthdays[newname] + ' is the birthday of ' + newname)
+
     else:
-        print('I do not hav ethe information for ' + name)
+        print('I do not hav ethe information for ' + newname)
+
         #This will take info under that name
         print('What is the birthday')
         baby = input()
-        birthdays[name] = baby
+        birthdays[newname] = baby
+
         #And, databse is updated now
         print('Birthdays database updated')
+
+#Your complete birthday list
+for k, v in birthdays.items():
+    print(k + '  : ' + str(v))
